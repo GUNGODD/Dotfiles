16f2bd390fb8562b5749685e90fc85053315eb65
GUNGODD <chauhanadityac01@gmail.com>
2024-05-19 18:54:41 +0530

    tmux config

--- init.lua
+++ init.lua
@@ -1,11 +1,14 @@
+
+
+-- Neovim options for tabs and indentation
 vim.cmd("set expandtab")
 vim.cmd("set tabstop=2")
 vim.cmd("set softtabstop=2")
 vim.cmd("set shiftwidth=2")
 
-
-local  lazypath = vim.fn.stdpath("data") .. "lazy/lazy.nvim"
-if not vim.loop.fs_stat(lazypath) then
+--lazy vim  plugin manager 
+local lazypath = vim.fn.stdpath("data") .. "/lazy/lazy.nvim"
+if not (vim.uv or vim.loop).fs_stat(lazypath) then
   vim.fn.system({
     "git",
     "clone",
@@ -15,23 +18,24 @@ if not vim.loop.fs_stat(lazypath) then
     lazypath,
   })
 end
-
 vim.opt.rtp:prepend(lazypath)
 
-local plugins = {
+local plugins  = {
 
-	
-
-	{ "catppuccin/nvim", name = "catppuccin", priority = 1000 },
-
-
+ -- colorscheme for editor 
+ { "catppuccin/nvim", name = "catppuccin", priority = 1000 },
+ {
+  'nvim-telescope/telescope.nvim', tag = '0.1.6',
+   dependencies = { 'nvim-lua/plenary.nvim' }
+  }
 
 }
-
 local opts = {}
 require("lazy").setup(plugins, opts)
+
+local builtin  = require("telescope.builtin")
+
+vim.keymap.nnoremap {'<C-p>', builtin.files}
+
 require("catppuccin").setup()
 vim.cmd.colorscheme "catppuccin"
-
-
-
